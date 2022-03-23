af67e8ed8dafcb322de2e0a8ae736c84c7cddff2
Dylan Hunt <Dylan.Hunt@4rhuskies.org>
2022-03-23 09:17:55 -0500

    end of hw1

--- src/app/folder/hw1_12/hw1prob12/hw1prob12.page.ts
+++ src/app/folder/hw1_12/hw1prob12/hw1prob12.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hw1prob12.page.scss'],
 })
 export class Hw1prob12Page implements OnInit {
-
+  upper
+  lower
+  length
+  start
   constructor() { }
 
   ngOnInit() {
+    let city = prompt('enter your favorite city')
+    this.upper = city.toUpperCase()
+    this.lower = city.toLowerCase()
+    this.length = city.length
+    this.start = city.charAt(0)
   }
 
 }
